3e15d7c4009f4ed58648b605c439db9d2286dc73
Arka Ghosh <71174892+aghoshpro@users.noreply.github.com>
2022-10-09 00:26:53 +0200

    count is not working

--- Kattis/aboveaverage.py
+++ Kattis/aboveaverage.py
@@ -1,24 +1,36 @@
-# Generate a random number between 0 and 9
 from audioop import avg
 import random
 
 sum = 0
 avgr = 0
+count = 0
 item = []
+
 # Number of Test Cases
 C = random.randint(1,5)
 print("C", C)
-print("--------------------------")
+print("-------------------------------------------------------")
+
 # the number of people in the class
 for j in range(0,C):
     N = random.randint(1, 5)
     print("N", N)
     for i in range(0,N):
         item.append(random.randint(1, 100))
-        sum=sum+random.randint(1, 100)
         sum=sum+item[i]
 
     avgr = sum/N
+    print(item)
     print("sum, avgr", sum, avgr)
     sum = 0
-    print(item)
+
+    for k in item:
+       print(k)
+       if k>avgr:
+        count+=1
+    
+    item = []
+
+    print("count", count)
+    # print("int, float",(count/N)*100,float(count/N)*100)
+    print("--------------------------")
---
+++ Kattis/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+    # print(count)
+    # print("int, float",(count/N)*100,float(count/N)*100)
