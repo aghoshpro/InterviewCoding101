5044372cf9fe012cdcdd21b3092fe239dfb55025
Arka Ghosh <71174892+aghoshpro@users.noreply.github.com>
2022-10-09 20:18:18 +0530

    Count is not working brooo

--- Kattis/aboveaverage.py
+++ Kattis/aboveaverage.py
@@ -7,17 +7,17 @@ count = 0
 item = []
 
 # Number of Test Cases
-C = random.randint(1,5)
+C = random.randint(1, 5)
 print("C", C)
 print("-------------------------------------------------------")
 
 # the number of people in the class
-for j in range(0,C):
+for j in range(0, C):
     N = random.randint(1, 5)
     print("N", N)
-    for i in range(0,N):
+    for i in range(0, N):
         item.append(random.randint(1, 100))
-        sum=sum+item[i]
+        sum = sum+item[i]
 
     avgr = sum/N
     print(item)
@@ -25,10 +25,10 @@ for j in range(0,C):
     sum = 0
 
     for k in item:
-       print(k)
-       if k>avgr:
-        count+=1
-    
+        print(k)
+        if k > avgr:
+            count += 1
+
     item = []
 
     print("count", count)
